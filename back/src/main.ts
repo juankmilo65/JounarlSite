ae10db0d20578083040f538d83084b5efdc3454d
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2020-10-18 11:48:26 +0300

    se configura swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const options = new DocumentBuilder()
+  .setTitle('Journal Documentation')
+  .setDescription('The journal API description')
+  .setVersion('1.0')
+  .addTag('Journal')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
