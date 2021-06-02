c4336e3b7b4d8862a11dcc0a3884251e91c16cd9
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2021-06-02 17:59:54 +0300

    classname package added and mobile menu changed.

--- front/src/components/common/useHeader.tsx
+++ front/src/components/common/useHeader.tsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Menu from './useMenu'
 import '../../styles/header.css'
 import {GiHamburgerMenu} from 'react-icons/gi';
+import classNames from 'classnames';
 
 
 
 export default function useHeader(): JSX.Element {
 
-    const updateHamburgerStyle = ()=>{
-        document.body.classList.toggle('nav-is-toggled');       
-    }
-    
+    const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+    const active = classNames('mobile-menu', {
+      open: mobileNavOpen,
+    });
+
     return (
-        <header >
-            <h1 className="title">RepoDoc</h1>
-            <Menu />
-            <div id="ham" className="hamburger"  onClick={updateHamburgerStyle}>
-                <GiHamburgerMenu />
+        <>
+            <header >
+                <h1 className="title">RepoDoc</h1>
+                <div id="ham" className="hamburger" 
+                onClick={() => {
+                    setMobileNavOpen((mobileNavOpen) => !mobileNavOpen);
+                }} >
+                    <GiHamburgerMenu />
+                </div>
+                <div className="menu">
+                    <Menu />
+                </div>
+            </header>
+            <div className="mobileMenu">
+                <Menu />
             </div>
-        </header>
+        </>
     )
 }
