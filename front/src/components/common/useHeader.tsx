ab5f1a2f08d3ad87d4703c628aa5f1b73d30a97d
Juan Camilo Morales Niño <juankmilo65@hotmail.com>
2021-06-02 15:20:00 +0300

    Hamburger menu.

--- front/src/components/common/useHeader.tsx
+++ front/src/components/common/useHeader.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Menu from './useMenu'
 import '../../styles/header.css'
+import {GiHamburgerMenu} from 'react-icons/gi';
+
+
 
 export default function useHeader(): JSX.Element {
+
+    const updateHamburgerStyle = ()=>{
+        document.body.classList.toggle('nav-is-toggled');       
+    }
+    
     return (
         <header >
             <h1 className="title">RepoDoc</h1>
             <Menu />
+            <div id="ham" className="hamburger"  onClick={updateHamburgerStyle}>
+                <GiHamburgerMenu />
+            </div>
         </header>
     )
 }
